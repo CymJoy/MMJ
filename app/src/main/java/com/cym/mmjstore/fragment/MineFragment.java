727b3822a3b045129fc8039a970d59bb50fa8c21
chenyingmiao <2740433420@qq.com>
2017-12-05 18:36:43 +0800

    首页，热门，分类

---
+++ app/src/main/java/com/cym/mmjstore/fragment/MineFragment.java
@@ -0,0 +1,40 @@
+package com.cym.mmjstore.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cym.mmjstore.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MineFragment extends BaseFragment {
+
+
+    public MineFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mine, container, false);
+    }
+
+    @Override
+    public View createView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+}
